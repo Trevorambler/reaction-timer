72497c89837fe930eb6596440b5436fd57775169
Trevorambler <trevor.ambler@bartlettroofs.com>
2023-08-07 13:07:09 -0600

    Begining of challenge 1.

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,37 @@
 <template>
 <h1>Trevor's timer</h1>
+<button @click="start" :disabled="isPlaying">Play</button>
+<Block v-if="isPlaying" :delay="delay" @end="endGame"/>
+<p v-if="showResults">Reaction time: {{ score }} ms</p>
 </template>
 
 <script>
-
+import Block from './components/Block.vue'
 
 export default {
   name: 'App',
-  components: {
+  components: { Block },
+  data () {
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResults: false,
+    
+    }
+  },
+  methods: {
+    start () {
+       this.delay = 2000 + Math.random() * 5000
+       this.isPlaying = true
+       this.showResults = false
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime
+      this.isPlaying = false
+      this.showResults = true
 
+    }
   }
 }
 </script>
@@ -19,7 +42,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #444;
   margin-top: 60px;
 }
 </style>
